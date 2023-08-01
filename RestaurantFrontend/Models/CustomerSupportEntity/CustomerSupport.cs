249bbe27c6216c357a269e5b555ce20ad9914e20
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-01 18:17:50 +0100

    added Customer Support Entity and its Controller

---
+++ RestaurantFrontend/Models/CustomerSupportEntity/CustomerSupport.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantFrontend.Models.CustomerSupportEntity
+{
+    public class CustomerSupport
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
