f9a7d81397a653b55b305e73a0f3d2d686c8ee90
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-03 18:08:36 +0100

    added javascript functionalities

--- RestaurantFrontend/Models/CustomerSupportEntity/CustomerSupport.cs
+++ RestaurantFrontend/Models/CustomerSupportEntity/CustomerSupport.cs
@@ -1,11 +1,18 @@
-﻿namespace RestaurantFrontend.Models.CustomerSupportEntity
+﻿using Microsoft.Build.Framework;
+
+namespace RestaurantFrontend.Models.CustomerSupportEntity
 {
     public class CustomerSupport
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
         public string Message { get; set; } = string.Empty;
     }
 }
