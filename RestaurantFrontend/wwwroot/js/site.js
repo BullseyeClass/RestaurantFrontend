f0bcc719c64cbf578ac993e3ff776a01bb8181a2
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-07 00:26:50 +0100

    added scroll up functionalities

--- RestaurantFrontend/wwwroot/js/site.js
+++ RestaurantFrontend/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const slideUpElements = document.querySelectorAll('.slide-up-text');
+
+setInterval(() => {
+    slideUpElements.forEach(element => {
+        element.classList.toggle('hidden');
+    });
+}, 5000); // Toggle every 10 seconds (adjust the time as needed)
