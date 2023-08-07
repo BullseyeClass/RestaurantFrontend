1dcd80e45d45a56965231bb43dcfcc309b24c78f
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-07 02:07:53 +0100

    added hover effect on nav-three

--- RestaurantFrontend/wwwroot/js/site.js
+++ RestaurantFrontend/wwwroot/js/site.js
@@ -1,11 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-const slideUpElements = document.querySelectorAll('.slide-up-text');
+﻿const slideUpElements = document.querySelectorAll('.slide-up-text');
 
 setInterval(() => {
     slideUpElements.forEach(element => {
         element.classList.toggle('hidden');
     });
-}, 5000); // Toggle every 10 seconds (adjust the time as needed)
+}, 5000);
