70b6fdbdc6fdaf8627ce0fb8e871f22ddd3e26ed
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-07 15:02:54 +0100

    adjusted the navbar

--- RestaurantFrontend/wwwroot/js/site.js
+++ RestaurantFrontend/wwwroot/js/site.js
@@ -5,3 +5,44 @@ setInterval(() => {
         element.classList.toggle('hidden');
     });
 }, 5000);
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const showListBtn = document.getElementById("search");
+    const hiddenList = document.querySelector(".trending-content");
+
+    showListBtn.addEventListener("click", function () {
+        if (hiddenList.style.display === "none") {
+            hiddenList.style.display = "block";
+            slideDown(hiddenList);
+        } else {
+            hiddenList.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!hiddenList.contains(event.target) && event.target !== showListBtn) {
+            hiddenList.style.display = "none";
+        }
+    });
+
+    function slideDown(element) {
+        let height = 0;
+        const maxHeight = element.scrollHeight;
+
+        const slideAnimation = setInterval(function () {
+            height += 5;
+            if (height <= maxHeight) {
+                element.style.height = height + "px";
+            } else {
+                clearInterval(slideAnimation);
+                element.style.overflow = "visible";
+            }
+        }, 10);
+    }
+});
+
