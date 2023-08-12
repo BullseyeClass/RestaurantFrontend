d7c56ab6b3dacd4cb19727b14a83d4961162c605
Ayodara200 <Daramola.A@Bullseyemanpower.com>
2023-08-12 14:26:08 +0100

    added neccessary method to the start your cart controller

--- RestaurantFrontend/Controllers/StartYourCart.cs
+++ RestaurantFrontend/Controllers/StartYourCart.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantFrontend.Models;
+using RestaurantFrontend.Repository.Interface;
+using System.Collections.Generic;
 
 namespace RestaurantFrontend.Controllers
 {
+
     public class StartYourCart : Controller
     {
+
+        private readonly IGettingMostPopularItem _gettingMostPopularItemsFromDB;
+
+        public StartYourCart(IGettingMostPopularItem gettingJobsFromDB)
+        {
+            _gettingMostPopularItemsFromDB = gettingJobsFromDB;
+        }
+
+        [Route("StartYourCart")]
         public IActionResult Index()
         {
-            return View();
+
+            var StartYourCart = _gettingMostPopularItemsFromDB.GetMostPopularItemFromDataSource();
+
+
+            return View(StartYourCart.ToList());
         }
+
+        [Route("Deals")]
+        public IActionResult Deals()
+        {
+            var products = _gettingMostPopularItemsFromDB.GetMostPopularItemFromDataSource().Where(x => x.Tag == "Deals");
+
+
+
+            return View("Index", products);
+        }
+
+
+
+
     }
 }
+
