03e12568283419b9f11b3d33a3a4f4a3dcc9cc9d
Ayodara200 <Daramola.A@Bullseyemanpower.com>
2023-08-12 14:22:35 +0100

    added start cart class

---
+++ RestaurantFrontend/Controllers/StartYourCart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantFrontend.Controllers
+{
+    public class StartYourCart : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
