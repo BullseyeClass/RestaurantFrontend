df3cb06fbd8531285a70439842bb7a8b98046d07
Emmanuel Ogidan <88818922+Emogibam@users.noreply.github.com>
2023-08-14 08:07:31 +0100

    cosumming locally

--- RestaurantFrontend/Controllers/RegistrationPage/RegistrationController.cs
+++ RestaurantFrontend/Controllers/RegistrationPage/RegistrationController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantFrontend.Models.RegistrationPage;
 
 namespace RestaurantFrontend.Controllers.RegistrationPage
 {
     public class RegistrationController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _baseUrl;
+        public RegistrationController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            _baseUrl = _configuration["AppSettings:BaseUrl"];
+        }
 
 
 
@@ -15,20 +24,31 @@ namespace RestaurantFrontend.Controllers.RegistrationPage
         }
 
 
-        public ActionResult Index()
-        {
-            return View(new RegistrationController());
-        }
-
 
         [HttpPost]
-        public ActionResult Index(RegistrationController model)
+        public async Task<IActionResult> RegisteringUser(Registration registrationViewModel)
         {
-            if (ModelState.IsValid)
+            using (var httpClient = new HttpClient())
             {
-                return RedirectToAction("RegistrationSuccess");
+
+                var json = JsonConvert.SerializeObject(registrationViewModel);
+                var requestBody = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync($"{_baseUrl}/api/Customer/register", requestBody);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    // Process the response from the API
+                    return View();
+
+                }
+                else
+                {
+                    // Handle error case
+                    return View();
+
+                }
             }
-            return View(model);
         }
 
         public ActionResult RegistrationSuccess()
--- RestaurantFrontend/Models/RegistrationPage/Registration.cs
+++ RestaurantFrontend/Models/RegistrationPage/Registration.cs
@@ -4,21 +4,19 @@ namespace RestaurantFrontend.Models.RegistrationPage
 {
     public class Registration
     {
-            [Required(ErrorMessage = "Username is required")]
-            public string UserName { get; set; }
-            
-            [Required(ErrorMessage = "Email is required")]
-            [EmailAddress(ErrorMessage = "Invalid email address")]
-            public string Email { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
 
-            [Required(ErrorMessage = "Password is required")]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
 
-            //[Required(ErrorMessage = "Confirm Password is required")]
-            //[Compare("Password", ErrorMessage = "Passwords do not match")]
-            //[DataType(DataType.Password)]
-            //public string ConfirmPassword { get; set; }
-        
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        public string Password { get; set; }
     }
+
 }
