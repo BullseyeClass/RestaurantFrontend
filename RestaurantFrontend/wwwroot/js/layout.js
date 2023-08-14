e976e62c887b382317b2f6a3af05f56d9b4bcc36
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-15 00:30:20 +0100

    fixed the faulty animations and click events on searchbar

---
+++ RestaurantFrontend/wwwroot/js/layout.js
@@ -0,0 +1,25 @@
+﻿const slideUpElement1 = document.getElementById('slide-up-element1');
+const slideUpElement2 = document.getElementById('slide-up-element2');
+
+
+setInterval(() => {
+    slideUpElement1.classList.toggle('hiddens');
+    slideUpElement2.classList.toggle('hiddens');
+
+}, 5000);
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("search");
+    const searchExtension = document.getElementById("trending-content");
+
+    searchInput.addEventListener("focus", function () {
+        searchExtension.style.display = "block";
+    });
+
+    searchInput.addEventListener("blur", function () {
+        searchExtension.style.display = "none";
+    });
+});
--- RestaurantFrontend/wwwroot/js/site.js
+++ RestaurantFrontend/wwwroot/js/site.js
@@ -1,73 +1,64 @@
 
 ﻿
-const dropdownItem = document.querySelector('.dropdown');
-const dropdownMenu = document.querySelector('.dropdown-menu');
+//const dropdownItem = document.querySelector('.dropdown');
+//const dropdownMenu = document.querySelector('.dropdown-menu');
 
-dropdownItem.addEventListener('mouseenter', () => {
-    dropdownMenu.style.display = 'block';
-});
+//dropdownItem.addEventListener('mouseenter', () => {
+//    dropdownMenu.style.display = 'block';
+//});
 
-dropdownItem.addEventListener('mouseleave', () => {
-    dropdownMenu.style.display = 'none';
-});
-
-
-
-﻿const slideUpElements = document.querySelectorAll('.slide-up-text');
-
-setInterval(() => {
-    slideUpElements.forEach(element => {
-        element.classList.toggle('hidden');
-    });
-}, 5000);
+//dropdownItem.addEventListener('mouseleave', () => {
+//    dropdownMenu.style.display = 'none';
+//});
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const showListBtn = document.getElementById("search");
-    const hiddenList = document.querySelector(".trending-content");
-    const trendingContainer = document.querySelector('#trend')
 
-    showListBtn.addEventListener("click", function () {
-        if (hiddenList.style.display === "none") {
-            hiddenList.style.display = "block";
-            trendingContainer.classList.add('addborder');
-            slideDown(hiddenList);
-        } else {
-            hiddenList.style.display = "none";
-            trendingContainer.classList.remove('addborder');
-        }
-    });
+//document.addEventListener("DOMContentLoaded", function () {
+//    const showListBtn = document.getElementById("search");
+//    const hiddenList = document.querySelector(".trending-content");
+//    const trendingContainer = document.querySelector('#trend')
 
-    document.addEventListener("click", function (event) {
-        if (!hiddenList.contains(event.target) && event.target !== showListBtn) {
-            hiddenList.style.display = "none";
-            trendingContainer.classList.remove('addborder');
-        }
-    });
+//    showListBtn.addEventListener("click", function () {
+//        if (hiddenList.style.display === "none") {
+//            hiddenList.style.display = "block";
+//            trendingContainer.classList.add('addborder');
+//            slideDown(hiddenList);
+//        } else {
+//            hiddenList.style.display = "none";
+//            trendingContainer.classList.remove('addborder');
+//        }
+//    });
 
-    function slideDown(element) {
-        let height = 0;
-        const maxHeight = element.scrollHeight;
+//    document.addEventListener("click", function (event) {
+//        if (!hiddenList.contains(event.target) && event.target !== showListBtn) {
+//            hiddenList.style.display = "none";
+//            trendingContainer.classList.remove('addborder');
+//        }
+//    });
 
-        const slideAnimation = setInterval(function () {
-            height += 5;
-            if (height <= maxHeight) {
-                element.style.height = height + "px";
-            } else {
-                clearInterval(slideAnimation);
-                element.style.overflow = "visible";
-            }
-        }, 10);
-    }
-});
+//    function slideDown(element) {
+//        let height = 0;
+//        const maxHeight = element.scrollHeight;
+
+//        const slideAnimation = setInterval(function () {
+//            height += 5;
+//            if (height <= maxHeight) {
+//                element.style.height = height + "px";
+//            } else {
+//                clearInterval(slideAnimation);
+//                element.style.overflow = "visible";
+//            }
+//        }, 10);
+//    }
+//});
 
 
 
 
 
-const divElement = document.getElementById("myDiv");
+//const divElement = document.getElementById("myDiv");
 
-divElement.addEventListener("click", () => {
-    divElement.focus();
-});
+//divElement.addEventListener("click", () => {
+//    divElement.focus();
+//});
 
