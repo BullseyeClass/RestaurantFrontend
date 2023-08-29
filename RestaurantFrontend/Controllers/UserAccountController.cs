0bda8f267525974c13ea8ac171aad6fd02212e13
Dairo Timileyin <timileyin4baze@gmail.com>
2023-08-29 08:26:11 +0100

    UPDATE

--- RestaurantFrontend/Controllers/UserAccountController.cs
+++ RestaurantFrontend/Controllers/UserAccountController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantFrontend.Models.Address;
+using RestaurantFrontend.Models.ApiResponses;
+using System.Security.Claims;
 
 namespace RestaurantFrontend.Controllers
 {
     [Authorize]
     public class UserAccountController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration;
+        private readonly string _baseUrl;
+
+        public UserAccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _baseUrl = _configuration["AppSettings:BaseUrl"];
+        }
+        public async Task<IActionResult> Index()
         {
             if (!User.Identity.IsAuthenticated)
             {
@@ -15,7 +27,42 @@ namespace RestaurantFrontend.Controllers
                 return RedirectToAction("Login", "Authentication");
             }
 
-            return View();
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var userId = HttpContext.User.FindFirst(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+                    HttpResponseMessage response = await httpClient.GetAsync($"{_baseUrl}/api/Customer/FullUserDetails/{userId}");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        List<UserDetails> userDetails = JsonConvert.DeserializeObject<List<UserDetails>>(responseBody);
+
+                        //var UserAddress = allAddress.Where(x => x.CustomerId == Guid.Parse(userId));
+
+                        //string errorMessage = TempData["SuccessMessage"] as string;
+
+                        //ViewBag.SuccessMessage = errorMessage;
+
+                        return View(userDetails);
+
+                    }
+
+                    else
+                    {
+                        //Handle error case
+                        return RedirectToAction("Index", "ErrorMessage");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                return RedirectToAction("Index", "ErrorMessage");
+
+
+            }
         }
     }
 }
